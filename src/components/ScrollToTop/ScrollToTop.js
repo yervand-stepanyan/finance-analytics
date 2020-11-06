6c17d071ee52937c80cbf4255f9b3494990ce8e8
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2020-11-06 19:25:10 +0100

    Add ScrollToTop component

---
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+function ScrollToTop({ children, history }) {
+  useEffect(() => {
+    const unListen = history.listen(() => {
+      window.scrollTo({
+        behavior: 'smooth',
+        left: 0,
+        top: 0,
+      });
+    });
+
+    return () => {
+      unListen();
+    };
+  }, [history]);
+
+  return <>{children}</>;
+}
+
+ScrollToTop.propTypes = {
+  children: PropTypes.array.isRequired,
+  history: PropTypes.object.isRequired,
+};
+
+export default ScrollToTop;
