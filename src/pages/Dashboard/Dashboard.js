3f91f75e863b25723cd8b7257dbbd31e1d0d15c7
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2020-11-13 15:24:04 +0100

    WIP field items styling in Dashboard.js and Dashboard.style.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
+import { FIELD_LIST } from '../../globals/constants';
 import { useStyles } from './Dashboard.style';
 
-function Dashboard() {
+function Dashboard({ currentUser }) {
   const classes = useStyles();
+  const { username } = currentUser;
+  const [fieldNavList, setFieldNavList] = useState(FIELD_LIST);
 
-  return <div className={classes.dashboardContainer}>Dashboard: user</div>;
+  const handleFieldSelect = name => {
+    const newFieldNavList = fieldNavList.map(field =>
+      field.name === name
+        ? {
+            ...field,
+            checked: true,
+          }
+        : {
+            ...field,
+            checked: false,
+          }
+    );
+
+    setFieldNavList(newFieldNavList);
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      handleFieldSelect();
+    }
+  };
+
+  return (
+    <div className={classes.dashboardContainer}>
+      <div className={classes.fieldNavWrapper}>
+        {fieldNavList.map(item => (
+          <div
+            className={`${classes.itemWrapper} ${
+              item.checked ? classes.checkedItemWrapper : ''
+            }`}
+            key={item.name}
+            onClick={() => handleFieldSelect(item.name)}
+            onKeyPress={e => handleKeyPress(e)}
+            role="button"
+            tabIndex={0}
+          >
+            <div className={`${item.checked ? classes.checkedItem : ''}`} />
+            <div className={classes.itemNameWrapper}>{item.name}</div>
+          </div>
+        ))}
+      </div>
+      <div>{`User: ${username}`}</div>
+    </div>
+  );
 }
+
+Dashboard.propTypes = {
+  currentUser: PropTypes.object.isRequired,
+};
 
 export default Dashboard;
--- src/pages/Dashboard/Dashboard.style.js
+++ src/pages/Dashboard/Dashboard.style.js
@@ -2,7 +2,58 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
   dashboardContainer: {
+    display: 'flex',
     minHeight: 'inherit',
-    padding: `0 ${theme.customSpacing.base}`,
+    flexDirection: 'column',
+    '@media only screen and (min-width: 600px)': {
+      flexDirection: 'row',
+    },
+  },
+  fieldNavWrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    backgroundColor: theme.color.headerBackgroundColor,
+    boxSizing: 'border-box',
+    padding: `${theme.customSpacing.small} ${theme.customSpacing.base}`,
+    '@media only screen and (min-width: 600px)': {
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      minWidth: theme.size.fieldNavigationWidth,
+      padding: `${theme.customSpacing.base} ${theme.customSpacing.base} ${theme.customSpacing.small} 0`,
+    },
+  },
+  itemWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    cursor: 'pointer',
+    height: theme.size.fieldItemHeight,
+    outline: 'none',
+    '@media only screen and (min-width: 600px)': {
+      flexDirection: 'row',
+      marginBottom: theme.customSpacing.small,
+    },
+  },
+  checkedItemWrapper: {
+    cursor: 'default',
+  },
+  checkedItem: {
+    backgroundColor: theme.color.primaryColor,
+    borderBottomLeftRadius: theme.size.fieldItemFullSize,
+    borderBottomRightRadius: theme.size.fieldItemFullSize,
+    height: theme.size.fieldItemPartSize,
+    width: theme.size.fieldItemFullSize,
+    '@media only screen and (min-width: 600px)': {
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: theme.size.fieldItemFullSize,
+      borderTopRightRadius: theme.size.fieldItemFullSize,
+      height: theme.size.fieldItemFullSize,
+      width: theme.size.fieldItemPartSize,
+    },
+  },
+  itemNameWrapper: {
+    '@media only screen and (min-width: 600px)': {
+      marginLeft: theme.customSpacing.small,
+    },
   },
 }));
