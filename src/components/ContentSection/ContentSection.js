f0eaaae09a55ffd6fbe9ee098fa58b66e3426ad7
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2020-11-17 15:49:30 +0100

    Add TabBar component

--- src/components/ContentSection/ContentSection.js
+++ src/components/ContentSection/ContentSection.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Typography } from '@material-ui/core';
-
 import { useStyles } from './ContentSection.style';
+import TabBar from '../TabBar';
 
 function ContentSection({ handleTabKeyPress, handleTabSelect, tabList }) {
   const classes = useStyles();
@@ -11,24 +10,11 @@ function ContentSection({ handleTabKeyPress, handleTabSelect, tabList }) {
   return (
     <div className={classes.contentSectionContainer}>
       <div className={classes.tabAndCardsWrapper}>
-        <div className={classes.tabBarContainer}>
-          <div className={classes.tabBarWrapper}>
-            {tabList.map(tab => (
-              <div
-                className={`${classes.tabWrapper} ${
-                  tab.checked ? classes.checkedTab : ''
-                }`}
-                key={tab.title}
-                onClick={() => handleTabSelect(tab.title)}
-                onKeyPress={e => handleTabKeyPress(e)}
-                role="button"
-                tabIndex={0}
-              >
-                <Typography variant="subtitle1">{tab.title}</Typography>
-              </div>
-            ))}
-          </div>
-        </div>
+        <TabBar
+          handleTabKeyPress={handleTabKeyPress}
+          handleTabSelect={handleTabSelect}
+          tabList={tabList}
+        />
       </div>
     </div>
   );
---
+++ src/components/TabBar/TabBar.style.js
@@ -0,0 +1,38 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(theme => ({
+  tabBarContainer: {
+    backgroundColor: theme.color.tabBarBackgroundColor,
+    '@media only screen and (min-width: 600px)': {
+      borderRadius: theme.border.borderRadius,
+    },
+  },
+  tabBarWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    cursor: 'default',
+    height: theme.size.tabHeight,
+    'overflow-x': 'auto',
+    '@media only screen and (min-width: 600px)': {
+      justifyContent: 'space-around',
+    },
+  },
+  tabWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: `${theme.border.tabBorder.size} solid transparent`,
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderTop: `${theme.border.tabBorder.size} solid transparent`,
+    boxSizing: 'border-box',
+    color: theme.color.tabNotSelectedColor,
+    cursor: 'pointer',
+    height: 'inherit',
+    outline: 'none',
+    padding: `0 ${theme.customSpacing.small}`,
+  },
+  checkedTab: {
+    color: theme.color.titleTextColor,
+    borderBottom: `${theme.border.tabBorder.size} solid ${theme.color.primaryColor}`,
+  },
+}));
