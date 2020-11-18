d26c18fae9a3da9b8fbbaf74692ebb02f628cb35
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2020-11-18 13:24:19 +0100

    Add routes for finance in Routes.js

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,7 @@
 const ROUTES = {
-  dashboard: '/dashboard',
+  finance: '/finance',
+  financeDashboard: 'dashboard',
+  financeSignin: 'signin',
   home: '/',
   signin: '/signin',
   signup: '/signup',
