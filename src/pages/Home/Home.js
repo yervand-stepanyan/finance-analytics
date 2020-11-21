d6a4fc86b4320e3e0351ad1ac74940466f47a41d
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2020-11-21 16:45:14 +0100

    Refactor redirect to finance in Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
+import ROUTES from '../../routes';
 import { useStyles } from './Home.style';
 
-function Home() {
+function Home({ currentUser }) {
   const classes = useStyles();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (currentUser) {
+      history.push(ROUTES.finance);
+    }
+  }, []);
 
   return <div className={classes.homeContainer}>Home</div>;
 }
+
+Home.propTypes = {
+  currentUser: PropTypes.object.isRequired,
+};
 
 export default Home;
