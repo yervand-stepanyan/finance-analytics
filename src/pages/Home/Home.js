2059914de996ae6297df7da2e87a3a94d160d67e
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2020-11-23 19:10:36 +0100

    Add Home page styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import { Typography } from '@material-ui/core';
+
+import { HOME, IMAGE } from '../../globals/constants';
 import ROUTES from '../../routes';
 import { useStyles } from './Home.style';
 
@@ -15,7 +18,46 @@ function Home({ currentUser }) {
     }
   }, []);
 
-  return <div className={classes.homeContainer}>Home</div>;
+  return (
+    <div className={classes.homeContainer}>
+      <div className={classes.firstSectionWrapper}>
+        <div className={classes.textContentWrapper}>
+          <div>
+            <Typography color="primary" variant="h3">
+              {HOME.title}
+            </Typography>
+          </div>
+          <div className={classes.section1Wrapper}>
+            <Typography variant="h5">{HOME.section1}</Typography>
+          </div>
+          <div className={classes.section2Wrapper}>
+            <Typography variant="h5">{HOME.section2}</Typography>
+          </div>
+        </div>
+        <div className={classes.imageWrapper}>
+          <img
+            alt={IMAGE.finance1.title}
+            className={classes.image}
+            src={IMAGE.finance1.src}
+          />
+        </div>
+      </div>
+      <div className={classes.secondSectionWrapper}>
+        <div className={classes.image2Wrapper}>
+          <img
+            alt={IMAGE.finance2.title}
+            className={classes.image}
+            src={IMAGE.finance2.src}
+          />
+        </div>
+        <div className={classes.textContentWrapper}>
+          <div>
+            <Typography variant="h5">{HOME.section3}</Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 Home.propTypes = {
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -3,6 +3,67 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles(theme => ({
   homeContainer: {
     minHeight: 'inherit',
-    padding: `0 ${theme.customSpacing.base}`,
+    padding: `${theme.customSpacing.small}`,
+    '@media only screen and (min-width: 600px)': {
+      padding: `${theme.customSpacing.base}`,
+    },
+  },
+  firstSectionWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    '@media only screen and (min-width: 600px)': {
+      alignItems: 'center',
+      flexDirection: 'row',
+      padding: `${theme.customSpacing.xLarge}`,
+    },
+  },
+  textContentWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    '@media only screen and (min-width: 600px)': {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      textAlign: 'left',
+      width: '40%',
+    },
+  },
+  section1Wrapper: {
+    marginTop: theme.customSpacing.base,
+  },
+  section2Wrapper: {
+    marginTop: theme.customSpacing.base,
+  },
+  imageWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    height: '100%',
+    width: '100%',
+    '@media only screen and (min-width: 600px)': {
+      width: '80%',
+    },
+  },
+  image: {
+    width: 'inherit',
+  },
+  secondSectionWrapper: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    marginTop: theme.customSpacing.large,
+    '@media only screen and (min-width: 600px)': {
+      alignItems: 'center',
+      flexDirection: 'row',
+      marginTop: 0,
+      padding: `0 ${theme.customSpacing.xLarge}`,
+    },
+  },
+  image2Wrapper: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    height: '100%',
+    width: '100%',
+    '@media only screen and (min-width: 600px)': {
+      width: '80%',
+    },
   },
 }));
